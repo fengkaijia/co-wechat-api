9b97ecb3c620ffe44e3301c9efb4402031decedd
ikrong <lun.i@outlook.com>
2019-01-13 22:21:41 +0800

    添加d.ts文件

---
+++ base.d.ts
@@ -0,0 +1,109 @@
+/**
+ * generate by ikrong from ikrong.com
+ * @@datetime@@
+ */
+
+declare class AccessToken {
+    constructor(accessToken, expireTime)
+
+    /*!
+     * 检查AccessToken是否有效，检查规则为当前时间和过期时间进行对比
+     * Examples:
+     * ```
+     * token.isValid();
+     * ```
+     */
+    isValid: () => boolean
+}
+
+declare class API {
+    static AccessToken: AccessToken
+    /**
+   * 根据 appid 和 appsecret 创建API的构造函数
+   * 如需跨进程跨机器进行操作Wechat API（依赖access token），access token需要进行全局维护
+   * 使用策略如下：
+   * 1. 调用用户传入的获取 token 的异步方法，获得 token 之后使用
+   * 2. 使用appid/appsecret获取 token 。并调用用户传入的保存 token 方法保存
+   * Tips:
+   * - 如果跨机器运行wechat模块，需要注意同步机器之间的系统时间。
+   * Examples:
+   * ```
+   * var API = require('wechat-api');
+   * var api = new API('appid', 'secret');
+   * ```
+   * 以上即可满足单进程使用。
+   * 当多进程时，token 需要全局维护，以下为保存 token 的接口。
+   * ```
+   * var api = new API('appid', 'secret', async function () {
+   *   // 传入一个获取全局 token 的方法
+   *   var txt = await fs.readFile('access_token.txt', 'utf8');
+   *   return JSON.parse(txt);
+   * }, async function (token) {
+   *   // 请将 token 存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+   *   // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+   *   await fs.writeFile('access_token.txt', JSON.stringify(token));
+   * });
+   * ```
+   * @param {String} appid 在公众平台上申请得到的appid
+   * @param {String} appsecret 在公众平台上申请得到的app secret
+   * @param {AsyncFunction} getToken 可选的。获取全局token对象的方法，多进程模式部署时需在意
+   * @param {AsyncFunction} saveToken 可选的。保存全局token对象的方法，多进程模式部署时需在意
+   */
+    constructor(appid, appsecret, getToken, saveToken, tokenFromCustom)
+    /**
+     * 用于设置urllib的默认options * Examples:
+     * ```
+     * api.setOpts({timeout: 15000});
+     * ```
+     * @param {Object} opts 默认选项
+     */
+    setOpts: (opts) => void;
+    /**
+     * 设置urllib的hook
+     */
+    request: (url, opts, retry) => any
+    /*!
+    * 根据创建API时传入的appid和appsecret获取access token
+    * 进行后续所有API调用时，需要先获取access token
+    * 详细请看：<http://mp.weixin.qq.com/wiki/index.php?title=获取access_token> * 应用开发者无需直接调用本API。 * Examples:
+    * ```
+    * var token = await api.getAccessToken();
+    * ```
+    * - `err`, 获取access token出现异常时的异常对象
+    * - `result`, 成功时得到的响应结果 * Result:
+    * ```
+    * {"access_token": "ACCESS_TOKEN","expires_in": 7200}
+    * ```
+    */
+    getAccessToken: () => Promise<string>
+    /*!
+    * 需要access token的接口调用如果采用preRequest进行封装后，就可以直接调用。
+    * 无需依赖 getAccessToken 为前置调用。
+    * 应用开发者无需直接调用此API。
+    * Examples:
+    * ```
+    * await api.ensureAccessToken();
+    * ```
+    */
+    ensureAccessToken: () => Promise<string>
+    /**
+      * 用于支持对象合并。将对象合并到API.prototype上，使得能够支持扩展
+      * Examples:
+      * ```
+      * // 媒体管理（上传、下载）
+      * API.mixin(require('./lib/api_media'));
+      * ```
+      * @param {Object} obj 要合并的对象
+      */
+    mixin: () => void;
+
+
+    //--------//
+
+}
+
+declare namespace API { }
+
+export = API;
+
+export as namespace API;
---
+++ makedts.js
@@ -0,0 +1,66 @@
+const fs = require("fs")
+const files = fs.readdirSync("./lib/")
+const API = require("./index")
+
+const anotation = {};
+
+/**收集注释 */
+function CalcAnotation(file) {
+    let filecontent = fs.readFileSync(file).toString();
+    let contentArr = filecontent.split(/\n/g)
+    let start = false;
+    let comment = "";
+    contentArr.map(str => {
+        let line = str.trim()
+        if (line.match(/^\/\*/)) {
+            start = true;
+            comment = line + "\n";
+        } else if (line.match(/\*/) && start) {
+            comment += "    " + line + "\n";
+        } else if (line.match(/\*\/$/) && start) {
+            comment += "    " + line + "\n";
+        } else if (line.match(/exports\.([a-z]+)/i) && start) {
+            let fnName = line.match(/exports\.([a-z]+)/i)
+            let name = fnName[1]
+            anotation[name] = comment;
+            start = false;
+        }
+    })
+}
+
+files.map(file => CalcAnotation(`./lib/${file}`));
+
+/**获取函数参数 */
+function getParameterName(fn) {
+    if (typeof fn !== 'object' && typeof fn !== 'function') return;
+    const COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    const DEFAULT_PARAMS = /=[^,)]+/mg;
+    const FAT_ARROWS = /=>.*$/mg;
+    let code = fn.prototype ? fn.prototype.constructor.toString() : fn.toString();
+    code = code
+        .replace(COMMENTS, '')
+        .replace(FAT_ARROWS, '')
+        .replace(DEFAULT_PARAMS, '');
+    let result = code.slice(code.indexOf('(') + 1, code.indexOf(')')).match(/([^\s,]+)/g);
+    return result === null ? [] : result;
+}
+
+let dtsfile = ""
+
+for (let key in API.prototype) {
+    if (anotation[key]) {
+        dtsfile += "    " + anotation[key] + "\n\n"
+    }
+    dtsfile += "    " + `${key}:(${getParameterName(API.prototype[key]).join(",")})=>any \n\n`
+}
+/**替换模块描述 */
+let basefile = fs.readFileSync("./base.d.ts").toString()
+dtsfile = basefile.replace(`//--------//`, dtsfile);
+/**替换时间 */
+let date = new Date();
+dtsfile = dtsfile.replace(
+    "@@datetime@@",
+    `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+)
+
+fs.writeFileSync("./index.d.ts", dtsfile)
